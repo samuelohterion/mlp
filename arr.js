bbf9c171936c0b5ebb13b1f0a3947d403306e34b
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2017-02-24 07:09:45 +0100

    change something

--- arr.js
+++ arr.js
@@ -1,104 +1,105 @@
-function arr ( pAI_sizes, pFct_fct ) {
+function arr ( pI_size, pFct_fct = cst ( 0 ), pA_dst = [ ] ) {
+
+	for ( var i = 0; i < pI_size; ++ i )
+
+		pA_dst[ i ] = pFct_fct ( i );
+
+	return pA_dst;
+}
+
+function seq ( pI_begin, pI_end, pFct_fct = add ( 0 ), pA_dst = [ ] ) {
+
+	for ( var i = pI_begin; i < pI_end; ++ i )
+
+		pA_dst[ i - pI_begin ] = pFct_fct ( i );
+
+	return pA_dst;
+}
+
+function fun ( pFct_fct, pA_src, pA_dst = [ ] ) {
+
+	for ( var i = 0; i < pA_src.length; ++ i )
+
+		pA_dst[ i ] = pFct_fct ( pA_src[ i ] );
+
+	return pA_dst;
+}
+
+function rel ( pFct_fct, pA_src1, pA_src2, pA_dst = [ ] ) {
+
+	for ( var i = 0; i < pA_src1.length; ++ i )
+
+		pA_dst[ i ] = pFct_fct( pA_src1[ i ], pA_src2[ i ] );
+
+	return pA_dst;
+}
+
+function cum ( pFct_fct, pA_src, s = pA_src[ 0 ] ) {
 
 	var
-	idx = [ ];
+	i = 0;
 
-	for ( var i = 0; i < pAI_sizes.length; ++ i )
+	while ( ++ i < pA_src.length )
 
-		idx.push ( 0 );
+		s = pFct_fct( s, pA_src[ i ] );
 
-	function A ( p_fct, pI_idx = 0 ) {
+	return s;
+}
+
+
+
+// some useful functions
+
+function cst ( p_val ) {
+
+	var
+	val = p_val;
+
+	return function ( i ) {
+
+		return val;
+	};
+}
+
+function add ( p_val ) {
+
+	var
+	val = p_val;
+
+	return function ( i ) {
+
+		return val + i;
+	};
+}
+
+function pol ( pA_coeff ) {
+
+	var
+	coeff = pA_coeff.slice ( );
+
+	return function ( x ) {
 
 		var
-		rt = [ ];
+		r = 0,
+		e = -1,
+		p = 1;
 
-		idx[ pI_idx ] = 0;
+		while ( ++ e < coeff.length ) {
 
-		for ( var i = 0; i < pAI_sizes[ pI_idx ]; ++ i ) {
-
-			if ( pAI_sizes.length - 1 <= pI_idx )
-
-				rt.push ( p_fct( idx ) );
-
-			else
-
-				rt.push ( A ( p_fct, pI_idx + 1 ) );
-
-			++ idx[ pI_idx ];
+			r += coeff[ e ] * p;
+			p *= x;
 		}
 
-		return rt;
-	}
-
-	return A ( pFct_fct );
+		return r;
+	};
 }
 
-function all0 ( pFct_fct, pI_len, pA_dst ) { for ( var i = 0; i < pI_len; ++i ) pA_dst[ i ] = pFct_fct( pA_dst[ i ] ); }
+function sSet ( a ) { return a; }
 
-function all1 ( pFct_fct, pI_len, pA_dst, pA_src ) { for ( var i = 0; i < pI_len; ++i ) pA_dst[ i ] = pFct_fct( pA_dst[ i ], pA_src[ i ] ); }
+function sSqr ( a ) { return a * a; }
 
-function all2 ( pFct_fct, pI_len, pA_dst, pA_src1, pA_src2 ) { for ( var i = 0; i < pI_len; ++i ) pA_dst[ i ] = pFct_fct( pA_dst[ i ], pA_src1[ i ], pA_src2[ i ] ); }
+function rMul ( a, b ) { return a * b; }
 
-function cum1 ( pFct_fct, pI_len, pA_src ) { var dst = 0; for ( var i = 0; i < pI_len; ++i ) dst = pFct_fct( dst, pA_src[ i ] ); return dst; }
+function rAdd ( a, b ) { return a + b; }
 
-function cum2 ( pFct_fct, pI_len, pA_src1, pA_src2 ) { var dst = 0; for ( var i = 0; i < pI_len; ++i ) dst = pFct_fct( dst, pA_src1[ i ], pA_src2[ i ] ); return dst; }
-
-function pr ( p_text ) { if ( p_text instanceof Array ) for ( var i = 0; i < p_text.length; ++ i ) pr( p_text[ i ] ); else console.log ( p_text ); }
-
-
-function rel ( pFct_fct, pA_src1, pA_src2 ) {
-
-	var
-	r = [ ];
-
-	for ( var i = 0; i < pA_src1.length; ++ i ) {
-
-		r.push ( pFct_fct ( pA_src1[ i ], pA_src2[ i ] ) );
-	}
-
-	return r;
-}
-
-function cum ( pFct_fct, pA_src ) {
-
-	var
-	r = 0;
-
-	for ( var i = 0; i < pA_src.length; ++ i ) {
-
-		r = pFct_fct ( r, pA_src[ i ] ) );
-	}
-
-	return r;
-}
-
-
-function scalarProduct ( pA_src1, pA_src2 ) {
-
-	return cum (
-
-		function ( a, b ) {
-
-			a += b;
-		},
-		rel (
-
-			function ( a, b ) {
-
-				return a * b;
-			},
-			pA_src1,
-			pA_src2
-		)
-	)
-}
-
-function difference ( pA_src1, pA_src2 ) {
-
-	return rel (
-
-		function ( a, b ) { return a - b; }
-		pA_src1,
-		pA_src2
-	)
-}
+function round ( a, b = 2 ) { return Math.pow( 10, -b ) * Math.round ( a * Math.pow ( 10, b ) ); }
